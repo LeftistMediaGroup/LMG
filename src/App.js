4ce4de4081ba94846dff9fdf5f2ceb2b45b83c95
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2023-06-12 17:36:50 -0400

    Added Sidebar BN Sidebar not full screen

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import "chart.js/auto";
 import { Component } from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+
 import { allContext } from "./contexts.js";
 
 import Home from "./basic/Home.js";
@@ -13,9 +15,27 @@ import Dashboard from "./dashboard/Dashboard.js";
 function App() {
   return (
     <div className="App">
+      <div className="Sidebar">
+        <Sidebar
+        >
+          <Menu>
+            <MenuItem component={<Link to="/" />}> Home</MenuItem>
+            <MenuItem component={<Link to="/education" />}> Education</MenuItem>
+            <MenuItem component={<Link to="/volunteering" />}>
+              {" "}
+              Volunteering
+            </MenuItem>
+            <MenuItem component={<Link to="/dashboard" />}> Dashboard</MenuItem>
+          </Menu>
+        </Sidebar>
+      </div>
+
       <Routes>
-        <Route path="/education" element={<Education/>}></Route>
-        <Route path="/volunteering" element={<Volunteering></Volunteering>}></Route>
+        <Route path="/education" element={<Education />}></Route>
+        <Route
+          path="/volunteering"
+          element={<Volunteering></Volunteering>}
+        ></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route exact path="/" element={<Home></Home>} />/
       </Routes>
