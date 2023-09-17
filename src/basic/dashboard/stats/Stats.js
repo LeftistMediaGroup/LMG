84c4b73a5e8ba4f2bf43e71f755ef9bbb6cd6341
Daniel Tucker <DanielATuker@Outlook.com>
2023-09-16 22:31:56 -0400

    U Account R Context

--- src/basic/dashboard/stats/Stats.js
+++ src/basic/dashboard/stats/Stats.js
@@ -1,30 +1,18 @@
+import { Card, CardContent } from "@mui/material";
 import React, { Component } from "react";
 
-import { allContext } from "../../../contexts";
 export class Stats extends Component {
   constructor(props) {
     super(props);
-
-    let Context = allContext._currentValue;
-
-    this.state = { Context: Context };
   }
 
   render() {
     return (
-      <div className="row">
-        <div className="col-md-6 col-xl-4 grid-margin stretch-card">
-          <div className="card">
-            <div className="card-body">
-              <div className="d-flex flex-row justify-content-between">
-                <h4 className="card-title">Stats</h4>
-              </div>
-
-              <p> isLoggedin: </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Card>
+        <CardContent>
+          <h1>Stats</h1>
+        </CardContent>
+      </Card>
     );
   }
 }
