28fbc6862e5a4befc2851dcc924054df3ea8e814
Daniel Tucker <DanielATuker@Outlook.com>
2023-09-29 23:39:15 -0400

    U RSS

--- src/basic/dashboard/RSS/RSS.js
+++ src/basic/dashboard/RSS/RSS.js
@@ -15,7 +15,7 @@ export default class RSS extends Component {
     };
   }
 
-  getFeed() {
+  getFeed = () => {
     axios
       .get(
         `https://localhost-0.tail5cd89.ts.net/rss/get_rss`,
@@ -36,7 +36,8 @@ export default class RSS extends Component {
 
   componentDidMount() {
     this.getFeed();
-  }
+  };
+  
   render() {
     let feed = this.state.RSSRaw.map((story) => (
       <div class="col">
