8f9471978967e7f1941eed85150cdfa4752e038d
Daniel Tucker <DanielATuker@Outlook.com>
2023-09-29 22:03:33 -0400

    U RSS

--- src/basic/dashboard/RSS/RSS.js
+++ src/basic/dashboard/RSS/RSS.js
@@ -11,59 +11,43 @@ export default class RSS extends Component {
       SongLibrary: [],
       urls: [],
       queue: [],
+      RSSRaw: null,
     };
   }
 
   getFeed() {
     axios
-        .put(
-          `https://localhost-0.tail5cd89.ts.net/rss/get_rss`,
-          {
-            password: password,
-            email: email,
-          },
-          { withCredentials: true }
-        )
-        .then((result) => {
-          console.log(`Axios update: ${JSON.stringify(result, null, 2)}`);
-        })
-        .catch((err) => {
-          console.log(`Error: ${err}`);
-        });
+      .put(
+        `https://localhost-0.tail5cd89.ts.net/rss/get_rss`,
+        {
+          password: password,
+          email: email,
+        },
+        { withCredentials: true }
+      )
+      .then((result) => {
+        console.log(`Axios update: ${JSON.stringify(result, null, 2)}`);
+        this.setState({ RSSRaw: result });
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
   }
 
+  componentDidMount() {
+    this.getFeed();
+  }
   render() {
-    let Feed = [];
-
-    let feed = Object.values(Feed).map((song) => (
+    let feed = this.state.RSSRaw.map((story) => (
       <div class="col">
         <Card>
           <CardContent>
             <Card>
               <CardContent>
-                <h5>{song.Title}</h5>
+                <h5>{story.Title}</h5>
 
-                <Button>Play</Button>
                 <br />
 
-                <Button
-                  onClick={(song) => {
-                    this.state.queue.push(song.Title);
-                  }}
-                >
-                  Add to queue
-                </Button>
-                <br />
-
-                <p> Song Info </p>
-                <p> {song.Artist}</p>
-                <p>Song Summary</p>
-                <p>{song.Summary}</p>
-                <p>
-                  Lyrics:
-                  <br />
-                  Coming soon
-                </p>
               </CardContent>
             </Card>
           </CardContent>
